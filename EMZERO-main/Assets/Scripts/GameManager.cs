7eee3b941a6195e69ae7d1bf696b9bf7331c16b7
AlbertoGarrote <sugaryspider822@gmail.com>
2025-05-29 16:40:32 +0200

    REGAY, NUNCA SUPE COMO SE ESCRIBIA

--- EMZERO-main/Assets/Scripts/GameManager.cs
+++ EMZERO-main/Assets/Scripts/GameManager.cs
@@ -5,8 +5,13 @@ using Unity.VisualScripting;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
-
-
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using Unity.Networking.Transport.Relay;
+using Unity.Netcode.Transports.UTP;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using UnityEngine.UI;
 
 public class GameManager : NetworkBehaviour
 {
@@ -27,7 +32,8 @@ public class GameManager : NetworkBehaviour
     // Start is called before the first frame update
     public static GameManager Instance { get; private set; }
     [SerializeField] NetworkManager _networkManager;
-    [SerializeField] GameObject humanPrefab, zombiePrefab, onlineInfoText;
+    [SerializeField] GameObject humanPrefab, zombiePrefab, onlineInfoText, inputCodeObj;
+    [SerializeField] TMP_InputField inputCode;
     public List<ulong> clientIds;
     TMP_Text onlinePlayerNumberInfo, onlineTypeInfo;
     public string clientName;
@@ -56,7 +62,7 @@ public class GameManager : NetworkBehaviour
 
         _networkManager.OnClientConnectedCallback += OnPlayerConnect;
         _networkManager.OnClientDisconnectCallback += OnPlayerDisconnect;
-        //
+
         uniqueIdGenerator = GetComponent<UniqueIdGenerator>();
 
         SceneManager.sceneLoaded += OnSceneLoaded;
@@ -65,15 +71,19 @@ public class GameManager : NetworkBehaviour
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         onlineInfoText = GameObject.FindWithTag("onlineInfo");
+        inputCodeObj = GameObject.FindWithTag("codeText"); // cuadro de texto codigo
+        inputCode = inputCodeObj.GetComponentInChildren<TMP_InputField>();
         onlineTypeInfo = onlineInfoText.GetComponentsInChildren<TMP_Text>()[0];
         onlinePlayerNumberInfo = onlineInfoText.GetComponentsInChildren<TMP_Text>()[1];
+        
+
         if (!isStarted)
         {
             onlineInfoText.SetActive(false);
             isStarted = true;
         }
 
-        if(NetworkManager.Singleton.IsHost)
+        if (NetworkManager.Singleton.IsHost)
         {
 
             onlineTypeInfo.text = $"{clientName} [Servidor]";
@@ -95,33 +105,75 @@ public class GameManager : NetworkBehaviour
 
     }
 
-    public void startServer()
+    // RELAY
+    public async void startServer()
     {
         if (!serverStarted)
         {
-            clientName = uniqueIdGenerator.GenerateUniqueID();
-            _networkManager.StartHost();
-            serverStarted = true;
-            Debug.Log($"Iniciado el servidor");
-            onlineTypeInfo.text = $"{clientName} [Servidor]";
-           
+            //clientName = uniqueIdGenerator.GenerateUniqueID();
+            //_networkManager.StartHost();
+            //serverStarted = true;
+            //Debug.Log($"Iniciado el servidor");
+            //onlineTypeInfo.text = $"{clientName} [Servidor]";
+
+            try
+            {
+                await UnityServices.InitializeAsync();
+
+                if (!AuthenticationService.Instance.IsSignedIn)
+                {
+                    await AuthenticationService.Instance.SignInAnonymouslyAsync();
+                }
+                Allocation allocation = await RelayService.Instance.CreateAllocationAsync(10); // Este es el numero maximo de jugadores
+                string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+                Debug.Log(joinCode);
+                RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
+                _networkManager.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+                _networkManager.StartHost();
+            }
+            catch (RelayServiceException e)
+            {
+                Debug.Log(e);
+            }
+
             onlineInfoText.SetActive(true);
             onlinePlayerNumberInfo.gameObject.SetActive(true);
-            
+
         }
 
     }
-    public void StartHost()
-    { 
-    }
+
     public void startClient()
     {
-        if(!thisClientStarted)
+        if (!thisClientStarted)
         {
-            _networkManager.StartClient();
+            JoinRelay(inputCode.text);
 
         }
     }
+
+    public async void JoinRelay(string joinCode)
+    {
+        try
+        {
+            await UnityServices.InitializeAsync();
+
+            if (!AuthenticationService.Instance.IsSignedIn)
+            {
+                await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            }
+            Debug.Log("Uniendose al Relay " + joinCode);
+            JoinAllocation joinallocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+            RelayServerData relayServerData = new RelayServerData(joinallocation, "dtls");
+            _networkManager.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+            _networkManager.StartClient();
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
 
     public void disconectSelf()
     {
@@ -150,8 +202,8 @@ public class GameManager : NetworkBehaviour
 
             onlinePlayerNumberInfo.text = $"Jugadores: {clientIds.Count}/{minPlayerNumber}";
 
-            if(!thisClientHasName)
-            CreateClientID(clientId);
+            if (!thisClientHasName)
+                CreateClientID(clientId);
         }
     }
 
@@ -200,7 +252,7 @@ public class GameManager : NetworkBehaviour
     [ClientRpc]
     private void ConnectPlayerClientRpc(string message, ClientRpcParams clientRpcParams = default)
     {
-        if(!IsHost)
+        if (!IsHost)
         {
             clientName = message;
             onlineTypeInfo.text = message;
